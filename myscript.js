f04b4a71ee474f0211dbdae8697cbba6a0353c55
Atanu Mojumdar <91253222+AtanuMojumdar@users.noreply.github.com>
2023-01-10 00:15:08 +0530

    Added img,html,js,css

---
+++ myscript.js
@@ -0,0 +1,41 @@
+// alert("Ok")
+console.log("Ok")
+let a = document.getElementById('raj');
+console.log(a)
+let c = document.getElementById('chng')
+console.log(c)
+a.addEventListener('click',()=>{
+    c.innerHTML=`<p>Atanu Mojumdar</p><p>DOB: 19 Dec</p> <p><a href='https://www.instagram.com/atanu_mojumdar/?igshid=YmMyMTA2M2Y%3D' class="mlink">Insta</a></p>`
+})
+
+let m = document.getElementById('mono');
+console.log(m)
+m.addEventListener('click',()=>{
+    c.innerHTML=`<p>Manojit Karar</p><p>DOB: 8 Feb</p> <p><a href='https://www.instagram.com/mr_mk_04/?igshid=YmMyMTA2M2Y%3D' class="mlink">Insta</a></p>`
+})
+
+let p = document.getElementById('boka');
+console.log(p)
+p.addEventListener('click',()=>{
+    c.innerHTML=`<p>Pritam Bera</p><p>DOB: 2 Feb</p> <p><a href='https://www.instagram.com/___romeo__03/?igshid=YmMyMTA2M2Y%3D' class="mlink">Insta</a></p>`
+})
+let d = document.getElementById('manna');
+console.log(d)
+d.addEventListener('click',()=>{
+    c.innerHTML=`<p>Dibyendu Manna</p><p>DOB: 23 July</p> <p><a href='https://www.instagram.com/debendu2/?igshid=YmMyMTA2M2Y%3D' class="mlink">Insta</a></p>`
+})
+let b = document.getElementById('badal');
+console.log(b)
+b.addEventListener('click',()=>{
+    c.innerHTML=`<p>Badal Koley</p><p>DOB: 14 July</p> <p><a href='https://www.instagram.com/the_name_is_badal/?igshid=YmMyMTA2M2Y%3D' class="mlink">Insta</a></p>`
+})
+let b2 = document.getElementById('babu');
+console.log(b2)
+b2.addEventListener('click',()=>{
+    c.innerHTML=`<p>Shuvashish Bera</p><p>DOB: 29 Sept</p> <p><a href='https://www.instagram.com/sh0wgun_/?igshid=YmMyMTA2M2Y%3D' class="mlink">Insta</a></p>`
+})
+let s = document.getElementById('dubr');
+console.log(s)
+s.addEventListener('click',()=>{
+    c.innerHTML=`<p>Shubham Ghadai</p><p>DOB: 14 Oct</p> <p><a href='https://www.instagram.com/i___am_shubham/?igshid=YmMyMTA2M2Y%3D' class="mlink">Insta</a></p>`
+})
